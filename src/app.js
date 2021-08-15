44c9b49b20e8be53a8216d857cc92055d7fa6434
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-08-15 14:47:50 -0500

    [Style] Header styles

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
-console.log('Hola mundo!!!');
+import hamburguerMenu from './js/hamburguer-menu.js';
+
+document.addEventListener('DOMContentLoaded', (e) => {
+  hamburguerMenu('#header-btn', '.header-links', '.links-container a');
+});
